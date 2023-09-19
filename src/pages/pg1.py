7b856875700af5f2e963730a64e67539da008de7
RafaelVieira12 <rafael11934@hotmail.com>
2023-09-19 13:19:45 +0100

    DashTools Automatic Push - 09/19/2023 @ 13:19:45

--- src/pages/pg1.py
+++ src/pages/pg1.py
@@ -48,7 +48,8 @@ card_icon = {
 indicator_height = 150
 
 #======== Reading and Cleaning The Data =========#
-df = pd.read_csv('datasets/EDUCABIZ.csv')
+url = 'https://raw.githubusercontent.com/RafaelVieira13/EDUCABIZ_PROJECT/main/datasets/EDUCABIZ.csv'
+df = pd.read_csv(url)
 
 # Missing Values is equal to 0 so let´s replace all the missing values by 0
 df.fillna(0,inplace=True)
